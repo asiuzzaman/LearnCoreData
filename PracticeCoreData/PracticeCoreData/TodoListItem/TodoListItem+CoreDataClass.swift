b58621247423b8b329e32f688cb1dd329b6f8af9
asiuzzaman <zaman1304006@gmail.com>
2024-12-30 11:36:38 +0600

    Setup core data part

---
+++ PracticeCoreData/PracticeCoreData/TodoListItem/TodoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoListItem+CoreDataClass.swift
+//  PracticeCoreData
+//
+//  Created by Md. Asiuzzaman on 30/12/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoListItem)
+public class TodoListItem: NSManagedObject {
+
+}
--- PracticeCoreData/PracticeCoreData/ViewController.swift
+++ PracticeCoreData/PracticeCoreData/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // Core Data
+    func getAllItems() {
+        do {
+           try context.fetch(TodoListItem.fetchRequest())
+        }
+        catch {
+            print("Error fetching data")
+        }
+    }
+
+    func createItem(name: String) {
+        let item = TodoListItem(context: context)
+        item.name = name
+        item.createdAt = Date()
+
+        do {
+            try context.save()
+        } catch {
+            print("Error while saving data")
+        }
+    }
+
+    func deleteItem(item: TodoListItem)
+    {
+        context.delete(item)
+
+        do {
+            try context.save()
+        } catch {
+            print("Error while Deleting data")
+        }
+
+    }
+
+    func updateItem(item: TodoListItem, name: String) {
+        item.name = name
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error while Deleting data")
+        }
+    }
 
 }
 
