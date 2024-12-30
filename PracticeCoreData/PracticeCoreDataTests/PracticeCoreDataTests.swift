5f747523ab69591dbc2a890df3b8a4a01cf81b10
asiuzzaman <zaman1304006@gmail.com>
2024-12-30 10:47:52 +0600

    Create core-data project

---
+++ PracticeCoreData/PracticeCoreDataTests/PracticeCoreDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeCoreDataTests.swift
+//  PracticeCoreDataTests
+//
+//  Created by Md. Asiuzzaman on 30/12/24.
+//
+
+import Testing
+@testable import PracticeCoreData
+
+struct PracticeCoreDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
